c0d36033c3ed032dc3176a01bc0bf8b3f93c1435
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-06 23:33:15 -0300

    Create StudentService.java

---
+++ src/main/java/com/example/universityexample/universityexample/student/StudentService.java
@@ -0,0 +1,42 @@
+package com.example.universityexample.universityexample.student;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@CacheConfig(cacheNames = "product-cache")
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Cacheable
+    public Student getStudent(Long id){
+        return studentRepository.findById(id).orElseThrow(() ->
+        new RuntimeException("No such student found with id"));
+    }
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @CacheEvict
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+    @CachePut(key = "#id")
+    public Student updateStudent(String id, Student student) {
+        return studentRepository.save(student);
+    }
+}
