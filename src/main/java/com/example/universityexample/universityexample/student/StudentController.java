1a7d93d14c14ce276dee37c382e18a69d3d0419d
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-06 23:34:54 -0300

    Update StudentController and add new service

--- src/main/java/com/example/universityexample/universityexample/student/StudentController.java
+++ src/main/java/com/example/universityexample/universityexample/student/StudentController.java
@@ -1,27 +1,33 @@
 package com.example.universityexample.universityexample.student;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/student")
+@RequestMapping("/api/v1/students")
 @RequiredArgsConstructor
+@Slf4j
 public class StudentController {
 
-    private final StudentRepository studentRepository;
+    private final StudentService studentService;
 
     @GetMapping
     public List<Student> findAllStudents() {
-        return studentRepository.findAll();
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{studentID}")
+    public Student getCustomerByID(@PathVariable("studentID") String studentID) {
+        log.info("g=fetching customer with id {}", studentID);
+        return studentService.getStudent(Long.getLong(studentID));
     }
 
     @PostMapping
     public Student postNewStudent(Student newStudent) {
-        return studentRepository.save(newStudent);
+        log.info("Saving new customer");
+        return studentService.addStudent(newStudent);
     }
 }
