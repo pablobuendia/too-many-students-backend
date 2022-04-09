44d70437c79edaccaf014b42369873c500a132c6
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-09 14:54:58 -0300

    Add Address sample data

--- src/main/java/com/example/universityexample/universityexample/UniversityExampleApplication.java
+++ src/main/java/com/example/universityexample/universityexample/UniversityExampleApplication.java
@@ -1,5 +1,7 @@
 package com.example.universityexample.universityexample;
 
+import com.example.universityexample.universityexample.address.Address;
+import com.example.universityexample.universityexample.address.AddressRepository;
 import com.example.universityexample.universityexample.student.Student;
 import com.example.universityexample.universityexample.student.StudentRepository;
 import com.example.universityexample.universityexample.university.University;
@@ -11,6 +13,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 @RequiredArgsConstructor
 @Slf4j
@@ -18,6 +22,7 @@ public class UniversityExampleApplication implements CommandLineRunner {
 
 	private final StudentRepository studentRepository;
 	private final UniversityRepository universityRepository;
+	private final AddressRepository addressRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityExampleApplication.class, args);
@@ -29,6 +34,11 @@ public class UniversityExampleApplication implements CommandLineRunner {
 
 		val uni = University.builder().name("MIT").build();
 		universityRepository.save(uni);
+
+		val addressPablo = Address.builder().lineStreet1("Belgrano 111").build();
+		val addressMaria = Address.builder().lineStreet1("Guemes 111").build();
+		val addressJuana = Address.builder().lineStreet1("Paz 111").build();
+		addressRepository.saveAll(List.of(addressPablo, addressMaria, addressJuana));
 
 		studentRepository.save(Student.builder().firstName("Pablo").lastName("Buendia").university(uni).build());
 		studentRepository.save(Student.builder().firstName("Maria").lastName("Rodriguez").university(uni).build());
