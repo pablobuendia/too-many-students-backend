35cdd79a6ebd8cea34b179bf5cc51fa3ba6b4bf4
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-09 00:41:26 -0300

    Add new fields and manytoone relationship with university to Student entity

--- src/main/java/com/example/universityexample/universityexample/student/Student.java
+++ src/main/java/com/example/universityexample/universityexample/student/Student.java
@@ -1,23 +1,32 @@
 package com.example.universityexample.universityexample.student;
 
+import com.example.universityexample.universityexample.university.University;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table
-@Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Student {
 
     @Id
-    private String id;
-    private String name;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
 
+    @ManyToOne
+    @JoinColumn(name = "universityId")
+    private University university;
 }
