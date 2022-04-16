858473efb2aeeebc593fc40d0ac31466b93c0ed1
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-16 16:35:01 -0300

    Add security configuration

---
+++ src/main/java/com/example/universityexample/configuration/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.universityexample.configuration;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@ConditionalOnProperty(name = "com.pablo.security.enabled", havingValue = "true")
+@Log4j2
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll();
+    }
+
+}
