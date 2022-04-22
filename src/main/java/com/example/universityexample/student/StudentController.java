7145b002170df84c65bf4d7d135da05ca09e2860
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-21 22:49:03 -0300

    add swagger api info

--- src/main/java/com/example/universityexample/student/StudentController.java
+++ src/main/java/com/example/universityexample/student/StudentController.java
@@ -1,5 +1,8 @@
 package com.example.universityexample.student;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +13,19 @@ import java.util.List;
 @RequestMapping("/api/v1/student")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "StudentController", description = "Student controller")
 public class StudentController {
 
     private final StudentService studentService;
 
+    @Operation(summary = "Get all students", description = "Get all students, including addresses and university")
     @GetMapping
     List<StudentDto> findAllStudents() {
         return studentService.getAllStudents();
     }
 
     @GetMapping("/{studentID}")
-    StudentDto getCustomerByID(@PathVariable("studentID") String studentID) {
+    StudentDto getCustomerByID(@Parameter(description = "Student ID") @PathVariable("studentID") String studentID) {
         log.info("g=fetching customer with id {}", studentID);
         return studentService.getStudent(Long.parseLong(studentID));
     }
