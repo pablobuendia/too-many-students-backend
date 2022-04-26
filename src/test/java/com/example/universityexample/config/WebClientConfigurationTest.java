28a93933ae3abb6415dbf7a8da4898c15f1fbf58
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-25 23:53:01 -0300

    Add webclienttestconfguration

---
+++ src/test/java/com/example/universityexample/config/WebClientConfigurationTest.java
@@ -0,0 +1,34 @@
+package com.example.universityexample.config;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
+public class WebClientConfigurationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+    @TestConfiguration
+    public static class WebClientTestConfiguration {
+        private static final Logger LOGGER = LoggerFactory.getLogger(WebClientTestConfiguration.class);
+
+        @Bean
+        public WebClient getWebClient(final WebClient.Builder builder) {
+            WebClient webClient = builder
+                    .baseUrl("http://localhost")
+                    .build();
+            LOGGER.info("WebClient created during Test, using class {}", webClient);
+            return webClient;
+        }
+    }
+}
