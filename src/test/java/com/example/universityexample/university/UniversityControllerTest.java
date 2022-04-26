3ea3221e7eac23b45f3b15848d1016d447743872
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-26 00:07:40 -0300

    Create UniversityControllerTest.java

---
+++ src/test/java/com/example/universityexample/university/UniversityControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.universityexample.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class UniversityControllerTest {
+
+    @Test
+    void getAllUniversities() {
+    }
+
+    @Test
+    void getUniversity() {
+    }
+
+    @Test
+    void postNewUniversity() {
+    }
+}
