aa92b48fca1534ba19568e34ed0dab00d078f0a5
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-27 00:14:51 -0300

    Add new tests

--- src/test/java/com/example/universityexample/university/UniversityControllerTest.java
+++ src/test/java/com/example/universityexample/university/UniversityControllerTest.java
@@ -1,19 +1,40 @@
 package com.example.universityexample.university;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@WebMvcTest
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@ExtendWith(MockitoExtension.class)
 class UniversityControllerTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UniversityControllerTest.class);
+
+    @Mock
+    private UniversityRepository universityRepository;
+
+    @Mock
+    private UniversityService universityService;
+
+    @InjectMocks
+    private UniversityController universityController;
+
+    @BeforeEach()
+    void initUseCase() {
+        LOGGER.info("Before each, init the use case");
+    }
 
     @Test
     void getAllUniversities() {
+        universityController.getAllUniversities();
     }
 
     @Test
     void getUniversity() {
+        universityController.getUniversity("1");
     }
 
     @Test
