df53c03608cf93daf06cad3c5f64d98e91c2d8e6
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-28 00:01:48 -0300

    Create MVC test for UniversityController

--- src/test/java/com/example/universityexample/university/UniversityControllerTest.java
+++ src/test/java/com/example/universityexample/university/UniversityControllerTest.java
@@ -1,26 +1,50 @@
 package com.example.universityexample.university;
 
+import com.example.universityexample.address.AddressRepository;
+import com.example.universityexample.address.city.CityRepository;
+import com.example.universityexample.address.country.CountryRepository;
+import com.example.universityexample.student.StudentRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-@ExtendWith(MockitoExtension.class)
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UniversityController.class)
 class UniversityControllerTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UniversityControllerTest.class);
 
-    @Mock
-    private UniversityRepository universityRepository;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @Mock
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
     private UniversityService universityService;
 
-    @InjectMocks
-    private UniversityController universityController;
+    @MockBean
+    private UniversityRepository universityRepository;
+
+    @MockBean
+    private StudentRepository studentRepository;
+
+    @MockBean
+    private AddressRepository addressRepository;
+
+    @MockBean
+    private CityRepository cityRepository;
+
+    @MockBean
+    private CountryRepository countryRepository;
 
     @BeforeEach()
     void initUseCase() {
@@ -28,16 +52,18 @@ class UniversityControllerTest {
     }
 
     @Test
-    void getAllUniversities() {
-        universityController.getAllUniversities();
+    void getAllUniversities() throws Exception {
+        mockMvc.perform(get("/api/v1/university"))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void getUniversity() {
-        universityController.getUniversity("1");
+    void getUniversity() throws Exception {
+        
     }
 
     @Test
     void postNewUniversity() {
+
     }
 }
